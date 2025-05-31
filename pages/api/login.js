4e63cb0f868b45365092387640ef3478a58fde97
efebasol <efebasol@gmail.com>
2025-06-01 01:43:09 +0300

    1

--- pages/api/login.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const clientId = process.env.KICK_CLIENT_ID;
-  const redirectUri = process.env.REDIRECT_URI;
-
-  const authorizeUrl = `https://kick.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=chat:write%20user:read`;
-
-  res.redirect(authorizeUrl);
-}
